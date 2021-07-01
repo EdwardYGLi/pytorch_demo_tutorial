ba4cd5c0f665cf34cc9e46e799489b208b1dc17b
Edward Li <edward.li@socure.com>
2021-07-01 00:12:32 -0700

    wip training loop

--- train.py
+++ train.py
@@ -3,14 +3,16 @@ Created by edwardli on 6/30/21
 """
 
 import hydra
-import wandb
+import torch
+import datetime
+import os
 
+from datasets.dataset import DogsDataset
+from models.net import ConvolutionalAutoEncoder
+from utils import init_wandb, seed_random
 
-def init_wandb(cfg):
-    # initialize weights and biases.
-    wandb.init(project="torch tutorial", dir=cfg.paths.wandb_path, tags=cfg.params.tags)
-    wandb.tensorboard.patch(save=True, tensorboardX=False)
-    wandb.config.update(cfg)
+# prefer use Gpu for everything
+_device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
 
 
 def train():
@@ -22,8 +24,37 @@ def eval():
 
 
 @hydra.main(config_name='config', config_path='config')
-def main():
-    pass
+def main(cfg):
+
+    # init experiment in wandb
+    init_wandb(cfg)
+
+    # reproducibility
+    seed_random(cfg.seed)
+
+    # create model
+    model = ConvolutionalAutoEncoder()
+
+    # create output directory
+    now = datetime.datetime.now()
+    out_dir = os.path.join(cfg.paths.output_dir, now.strftime("%Y-%m-%d_%H_%M") + "_{}".format(model.__class__.__name__))
+    # make output directory
+    os.makedirs(out_dir, exist_ok=True)
+
+    # we can do reflection/importlib etc here as well, but being explicit is better for readability and understanding the code.
+    if cfg.optimizer.type == "adam":
+        optimizer = torch.optim.Adam(model.parameters, cfg.optimizer.lr, cfg.optimizer.betas, cfg.optimizer.eps)
+    elif cfg.optimizer.type == "adamw":
+        optimizer = torch.optim.AdamW(model.parameters, cfg.optimizer.lr, cfg.optimizer.betas, cfg.optimizer.eps,
+                                      cfg.optimizer.weight_decay)
+    elif cfg.optimizer.type == "sgd":
+        optimizer = torch.optim.SGD(model.parameters(), cfg.optimizer.lr, cfg.optimizer.momentum,
+                                    cfg.optimizer.dampening, cfg.optmizer.weight_decay, cfg.optimizer.nesterov)
+    else:
+        raise AssertionError("optimizer type not correct")
+
+    # create dataset
+    dataset = DogsDataset(cfg.dataset)
 
 
 if __name__ == '__main__':
--- utils.py
+++ utils.py
@@ -4,6 +4,32 @@ Created by edwardli on 6/30/21
 import cv2
 import numpy as np
 import torch
+import wandb
+import random
+import os
+
+
+def init_wandb(cfg):
+    # initialize weights and biases.
+    wandb.init(project="torch tutorial", dir=cfg.paths.wandb_path, tags=cfg.params.tags)
+    wandb.tensorboard.patch(save=True, tensorboardX=False)
+    wandb.config.update(cfg)
+
+
+def seed_random(seed: int):
+    """
+    Seeds random number generators with the same seed for reproducibility.
+    cudnn.deterministed = True changes cudnn background algorithm selection to be deterministic.
+        (https://stackoverflow.com/questions/56354461/reproducibility-and-performance-in-pytorch)
+    cudnn.benchmark = False (https://discuss.pytorch.org/t/how-should-i-disable-using-cudnn-in-my-code/38053/3)
+    reference: https://pytorch.org/docs/stable/notes/randomness.html
+    """
+    torch.random.manual_seed(seed)
+    random.seed(seed)
+    np.random.seed(seed)
+    os.environ['PYTHONHASHSEED'] = str(seed)
+    torch.backends.cudnn.deterministic = True
+    torch.backends.cudnn.benchmark = False
 
 
 def psnr(pred, target):
