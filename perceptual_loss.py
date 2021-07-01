58c5d1832845ec20a63063c68caf3d5e72762674
Edward Li <edward.li@socure.com>
2021-06-30 18:55:02 -0700

    add utilities and perceptual loss

--- perceptual_loss.py
+++ perceptual_loss.py
@@ -1,3 +1,89 @@
 """
 Created by edwardli on 6/30/21
 """
+import torch
+import torchvision.models.vgg as vgg
+
+device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
+
+
+class Normalization(torch.nn.Module):
+    def __init__(self, mean, std):
+        super(Normalization, self).__init__()
+        # .view the mean and std to make them [C x 1 x 1] so that they can
+        # directly work with image Tensor of shape [B x C x H x W].
+        # B is batch size. C is number of channels. H is height and W is width.
+        self.mean = mean.clone().detach().view(-1, 1, 1)
+        self.std = std.clone().detach().view(-1, 1, 1)
+
+    def forward(self, img):
+        # normalize img
+        return (img - self.mean) / self.std
+
+
+class PerceptualLoss(torch.nn.Module):
+    """
+    Evaluates how closely two images match by comparing a feature map.
+    """
+
+    def __init__(self):
+        super().__init__()
+        self.vgg = vgg.vgg13_bn(pretrained=True)
+
+        # freeze all model parameters
+        self.vgg.eval()
+        for param in self.vgg.parameters():
+            param.requires_grad = False
+        normalization_mean = torch.tensor([0.485, 0.456, 0.406]).to(device)
+        normalization_std = torch.tensor([0.229, 0.224, 0.225]).to(device)
+        self.normalization = Normalization(
+            normalization_mean, normalization_std)
+
+    def _extract_features(self, x):
+        """
+         Extract features for a given image
+         :param x: input
+         :return: feature dictionary
+         """
+        x = self.normalization(x)
+
+        for i in range(12):
+            x = self.vgg.features[i](x)
+        return x
+
+    def forward(self, view_a, view_b):
+        """
+        Calculates the mid level feature loss.
+        :param view_a: image A
+        :param view_b: image B
+        :return: perceptual loss
+        """
+        if view_a.shape != view_b.shape:
+            raise Exception(
+                "Input images to perceptual loss have mismatching shape")
+
+        if view_a.shape[1] == 1:
+            # Duplicate number of channels from 1 to 3 (grayscale to RGB
+            # representation)
+            view_a = torch.cat((view_a, view_a, view_a), 1)
+            view_b = torch.cat((view_b, view_b, view_b), 1)
+
+        p_loss = torch.mean(
+            torch.abs(
+                self._extract_features(view_a) -
+                self._extract_features(view_b)))
+
+        return p_loss
+
+
+_perceptual_loss_instance = None
+
+
+def perceptual_loss(view_a, view_b):
+    """
+    Functional wrapper for perceptual loss.
+    """
+    global _perceptual_loss_instance
+    if _perceptual_loss_instance is None:
+        _perceptual_loss_instance = PerceptualLoss().to(device)
+    return _perceptual_loss_instance(view_a, view_b)
--- utils.py
+++ utils.py
@@ -1,3 +1,51 @@
 """
 Created by edwardli on 6/30/21
 """
+import cv2
+import numpy as np
+import torch
+
+
+def psnr(pred, target):
+    # implement PSNR (peak to peak signal to noise ratio)
+    # between prediction and target here
+    mse = torch.mean((pred - target) ** 2)
+    return 20 * torch.log10(1 / torch.sqrt(mse))
+
+
+def ssim(pred, target):
+    # implement SSIM (structured similarity index),
+    # between prediction and target here\
+
+    # original dimension (b, c, h, w)
+    pred = pred.detach().cpu().permute(0, 2, 3, 1).numpy()
+    target = target.detach().cpu().permute(0, 2, 3, 1).numpy()
+    # dimension (b, h,w,c)
+    # we should implement this in pytorch native tensor operations for speed,
+    # but for readability we will do this the slow way (looping)
+    # for now.
+    ssim = 0
+    for i in range(pred.shape[0]):
+        p = pred[i]
+        t = target[i]
+        # constants for numerical stability
+        C1 = (0.01) ** 2
+        C2 = (0.03) ** 2
+
+        kernel = cv2.getGaussianKernel(11, 1.5)
+        window = np.outer(kernel, kernel.transpose())
+        mu1 = cv2.filter2D(p, -1, window)[5:-5, 5:-5]  # valid
+        mu2 = cv2.filter2D(t, -1, window)[5:-5, 5:-5]
+        mu1_sq = mu1 ** 2
+        mu2_sq = mu2 ** 2
+        mu1_mu2 = mu1 * mu2
+        sigma1_sq = cv2.filter2D(p ** 2, -1, window)[5:-5, 5:-5] - mu1_sq
+        sigma2_sq = cv2.filter2D(t ** 2, -1, window)[5:-5, 5:-5] - mu2_sq
+        sigma12 = cv2.filter2D(p * t, -1, window)[5:-5, 5:-5] - mu1_mu2
+
+        ssim_map = ((2 * mu1_mu2 + C1) * (2 * sigma12 + C2)) / (
+                (mu1_sq + mu2_sq + C1) * (sigma1_sq + sigma2_sq + C2)
+        )
+        ssim += ssim_map.mean()
+    # return batch avg ssim
+    return ssim / pred.shape[0]
