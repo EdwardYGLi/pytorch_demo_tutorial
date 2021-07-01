edaf301c7db0bbcdfad0132b7cd2ea3ef871348b
Edward Li <edward.li@socure.com>
2021-06-30 19:49:38 -0700

    add dataset

--- datasets/dataset.py
+++ datasets/dataset.py
@@ -2,15 +2,52 @@
 Created by edwardli on 6/30/21
 """
 
+import os
+from glob import glob
+
+import cv2
 from torch.utils.data.dataset import Dataset
+from torchvision import transforms
 
 
-class DogsDataset(Dataset):
-    def __init__(self, cfg):
-        pass
+class _DogsDatasetInternal(Dataset):
+    def __init__(self, path, pattern, trans=None):
+        self.files = glob.glob(os.path.join(path, pattern))
+        self.len = len(self.files)
+        # you can use torch transforms here, or write your own.
+        self.transforms = trans
 
     def __len__(self):
-        return
+        return len(self.files)
 
     def __getitem__(self, idx):
-        return
+        img = self.files[idx]
+        img = cv2.imread(img)
+
+        ## can add augmentations or any things here or use torch.transform to do it
+
+        if self.transforms:
+            img = self.transforms(img)
+        return img
+
+
+class DogsDataset:
+    def __init__(self, cfg):
+        # do some augmentations
+        train_transforms = transforms.Compose([
+            transforms.ToTensor(),
+            transforms.RandomResizedCrop(size=cfg.img_size),
+            transforms.ColorJitter(brightness=cfg.brightness_jitter, contrast=cfg.contrast_jitter,
+                                   saturation=cfg.saturation_jitter, hue=cfg.hue_jitter),
+            transforms.RandomHorizontalFlip(),
+            transforms.RandomVerticalFlip(),
+        ])
+
+        # for validation keep it simple.
+        validation_transforms = transforms.Compose([
+            transforms.ToTensor(),
+            transforms.Resize(size=cfg.img_size)
+        ])
+
+        self.training_data = _DogsDatasetInternal(cfg.train_path, cfg.file_pattern, trans=train_transforms)
+        self.validation_data = _DogsDatasetInternal(cfg.val_path, cfg.file_pattern, trans=validation_transforms)
--- models/net.py
+++ models/net.py
@@ -5,11 +5,13 @@ import torch.nn as nn
 
 
 class ConvolutionalAutoEncoder(nn.Module):
-    def __init__(self, cfg):
+    def __init__(self):
         super(ConvolutionalAutoEncoder, self).__init__()
-        # define our convolutional auto-encoder here without using convolution layers
+
+        # define our convolutional auto-encoder here using convolution layers
+        # can add parameters from config file as well. Our model is simple, so skipping that.
         self.encoder = nn.Sequential(
-            nn.Conv2d(1, 16, kernel_size=3, stride=1, padding=1, bias=False),
+            nn.Conv2d(3, 16, kernel_size=3, stride=1, padding=1, bias=False),
             nn.ReLU(),
             nn.MaxPool2d(kernel_size=2, stride=2),
             nn.Conv2d(16, 32, kernel_size=3, stride=1, padding=1, bias=False),
@@ -28,7 +30,7 @@ class ConvolutionalAutoEncoder(nn.Module):
             nn.Conv2d(32, 16, kernel_size=3, stride=1, padding=1, bias=False),
             nn.ReLU(),
             nn.UpsamplingNearest2d(scale_factor=2),
-            nn.Conv2d(16, 1, kernel_size=3, stride=1, padding=1, bias=False),
+            nn.Conv2d(16, 3, kernel_size=3, stride=1, padding=1, bias=False),
             nn.Sigmoid(),
         )
 
