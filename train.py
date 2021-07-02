75661b4b929ae5a7abe5b9d7be371c932f69d421
Edward Li <edward.li@socure.com>
2021-07-01 20:30:38 -0700

    training code loop done

--- train.py
+++ train.py
@@ -9,6 +9,7 @@ import wandb
 from hydra.utils import get_original_cwd
 from torch.utils.data import DataLoader
 from torch.utils.tensorboard import SummaryWriter
+from tqdm import tqdm
 
 from datasets.dataset import DogsDataset
 from models.net import ConvolutionalAutoEncoder
@@ -19,16 +20,51 @@ from utils import init_wandb, seed_random
 _device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
 
 
-def gen_validation_samples(writer, model, batch):
-    pass
+def gen_validation_samples(model, vis_batches, tboard, epoch):
+    for batch_idx, vis_batch in enumerate(vis_batches):
+        b, c, h, w = vis_batch.shape
+        outputs = model(vis_batch)
+
+        for i in range(b):
+            img = outputs[i].permute(1, 2, 0).cpu().numpy()
+            tboard.add_image("recon_b_{}_i_{}".format(batch_idx, i), img_tensor=img, global_step=epoch)
 
 
-def train(model, validation_data, loss_fn, optimizer):
-    pass
+def run_epoch(model, data_loader, loss_fn, optimizer=None):
+    loss_dict = {}
+    for key in loss_fn.keys():
+        loss_dict[key] = 0
+    loss_dict["total"] = 0
 
+    num_batches = len(data_loader)
 
-def eval(model, validation_data, loss_fn):
-    pass
+    for batch in data_loader:
+        batch = batch.to(_device)
+
+        if optimizer:
+            optimizer.zero_grad()
+
+        output = model(batch)
+
+        losses = []
+        for key, criterion in loss_fn:
+            curr_loss = criterion(output, batch)
+            losses.append(curr_loss)
+            loss_dict[key] += float(curr_loss)
+
+        total_loss = sum(losses)
+        loss_dict["total"] += float(total_loss)
+
+        if optimizer:
+            total_loss.backward()
+            optimizer.step()
+
+    # divide loss by number of batches for per batch loss as losses are batch
+    # mean errors
+    for key, value in loss_dict.items():
+        loss_dict[key] = value / num_batches
+
+    return loss_dict
 
 
 def get_optimizer(model, cfg):
@@ -77,6 +113,7 @@ def main(cfg):
 
     # create model
     model = ConvolutionalAutoEncoder()
+    model = model.to(_device)
     # weight and biases can watch the model and track gradients.
     wandb.watch(model)
 
@@ -86,48 +123,65 @@ def main(cfg):
     # get optimizer
     optimizer = get_optimizer(model, cfg)
 
+    # get scheduler
+    scheduler = None
+    # scheduler = get_scheduler(optimizer,cfg)
+
     # create dataset
-    dataset = DogsDataset(cfg.dataset)
+    dataset = DogsDataset(code_dir, cfg.dataset)
 
     train_loader = DataLoader(dataset.training_data, cfg.batch_size, shuffle=True)
     validation_loader = DataLoader(dataset.validation_data, cfg.batch_size, shuffle=False)
 
-    visualize_batch = next(iter(validation_loader))
+    visualize_batches = []
+    val_iter = iter(validation_loader)
+    # create some validation visualization batches.
+    for i in range(cfg.vis_batches):
+        visualize_batches.append(next(val_iter))
 
     # we can do reflection here as wel
     loss_fn = get_loss_fn(cfg)
 
     curr_best = 1.0E10
 
-    for epoch in range(cfg.epochs):
+    for epoch in tqdm(range(cfg.epochs)):
+        # step your scheduler
+        if scheduler:
+            scheduler.step()
+            tboard_writer.add_scalar("learning_rate", scheduler.get_lr()[0], epoch)
+
+        print("epoch {}".format(epoch))
         # pause autograd for speed for validation
         torch.set_grad_enabled(False)
         # do validation first
         model.eval()
         # loss some validation samples first
-        gen_validation_samples(model, visualize_batch, tboard_writer)
+        gen_validation_samples(model, visualize_batches, tboard_writer, epoch)
 
-        epoch_val_loss = eval(model, validation_loader, loss_fn)
+        # run an validation epoch
+        epoch_val_loss = run_epoch(model, validation_loader, loss_fn)
 
         if epoch_val_loss["total"] < curr_best:
-            torch.save(model.state_dict(),os.path.join(output_dir,"_best.pt"))
+            torch.save(model.state_dict(), os.path.join(output_dir, "_best.pt"))
 
         # log losses to tensorboard
         for key, value in epoch_val_loss.items():
-            tboard_writer.add_scalar(key,value,epoch)
+            print("val " + key, value)
+            tboard_writer.add_scalar(key, value, epoch)
 
         # resume autograd
         torch.set_grad_enabled(True)
         model.train()
 
-        epoch_train_loss = train(model, validation_loader, loss_fn, optimizer)
+        # run an training epoch
+        epoch_train_loss = run_epoch(model, validation_loader, loss_fn, optimizer)
 
         # log losses to tensorboard on epoch basis, can also log every few steps within the train function.
         for key, value in epoch_train_loss.items():
+            print("train " + key, value)
             tboard_writer.add_scalar(key, value, epoch)
 
-
-
+    torch.save(model.state_dict(), os.path.join(output_dir, "_final.pt"))
 
 
 if __name__ == '__main__':
